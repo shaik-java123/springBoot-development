f2db468b3c7e1d4484597871074305cce523b108
shaikhtarafdar <smainuddin@gmail.com>
2020-06-01 09:13:31 +0530

    Spring boot DB : Initial commit

---
+++ SpringBoot-Repository/src/test/java/com/example/SpringBootDB/SpringBootDbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.SpringBootDB;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootDbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
